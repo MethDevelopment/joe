dcad557e4b8fcbaf3dab364c4b089d3793d3d699
propruhh <propruhh@gmail.com>
2021-03-23 18:33:09 -0500

    add system wide settings

---
+++ joeprefs/SystemWideListController.h
@@ -0,0 +1,6 @@
+#import <Preferences/PSListController.h>
+
+@interface SystemWideListController : PSListController
+- (NSMutableArray *)specifiers;
+- (void)didTapRespring;
+@end
---
+++ joeprefs/SystemWideListController.m
@@ -0,0 +1,26 @@
+#import "SystemWideListController.h"
+#import <Foundation/Foundation.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSListController.h>
+
+@interface NSTask : NSObject
+@property NSString *launchPath;
+- (void)launch;
+@end
+
+@implementation SystemWideListController
+- (NSMutableArray *)specifiers {
+    if (_specifiers == nil) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"SystemWideListController" target:self];
+        UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"Respring" style:UIBarButtonItemStylePlain target:self action:@selector(didTapRespring)];
+        self.navigationItem.rightBarButtonItem = item;
+    }
+    return _specifiers;
+}
+
+- (void)didTapRespring {
+    NSTask *task = [[NSTask alloc] init];
+    task.launchPath = @"/usr/bin/sbreload";
+    [task launch];
+}
+@end
