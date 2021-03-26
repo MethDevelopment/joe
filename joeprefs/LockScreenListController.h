dbfff27e573698415e9d80e490e83c3678e1a7b3
propruhh <propruhh@gmail.com>
2021-03-25 21:44:24 -0500

    add lock screen settings

---
+++ joeprefs/LockScreenListController.h
@@ -0,0 +1,6 @@
+#import <Preferences/PSListController.h>
+
+@interface LockScreenListController : PSListController
+- (NSMutableArray *)specifiers;
+- (void)didTapRespring;
+@end
---
+++ joeprefs/LockScreenListController.m
@@ -0,0 +1,26 @@
+#import "LockScreenListController.h"
+#import <Foundation/Foundation.h>
+#import <Preferences/PSSpecifier.h>
+#import <Preferences/PSListController.h>
+
+@interface NSTask : NSObject
+@property NSString *launchPath;
+- (void)launch;
+@end
+
+@implementation LockScreenListController
+- (NSMutableArray *)specifiers {
+    if (_specifiers == nil) {
+        _specifiers = [self loadSpecifiersFromPlistName:@"LockScreenListController" target:self];
+        UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithTitle:@"Respring" style:UIBarButtonItemStylePlain target:self action:@selector(didTapRespring)];
+        self.navigationItem.rightBarButtonItem = item;
+    }
+    return _specifiers;
+}
+
+- (void)didTapRespring {
+    NSTask *task = [[NSTask alloc] init];
+    task.launchPath = @"/usr/bin/sbreload";
+    [task launch];
+}
+@end
